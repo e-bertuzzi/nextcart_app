67d0ccd8c030baed306b1b07958e0d51d5cdf316
unknown <e.bertuzzi003@studenti.unibs.it>
2025-07-11 19:37:09 +0200

    update file for bug correction

--- apps/website/jest.config.ts
+++ apps/website/jest.config.ts
@@ -5,6 +5,9 @@ export default {
     '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
     '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/react/babel'] }],
   },
+  transformIgnorePatterns: [
+    "/node_modules/(?!@cloudscape-design/components)"
+  ],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   coverageDirectory: '../coverage/website',
 };
